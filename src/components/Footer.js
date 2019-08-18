d8268b8523d743033f22f7efdd2fffbee2059a05
Jayjeet Chakraborty <jc.github@rediffmail.com>
2019-08-18 23:34:39 +0530

    Fixed footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import logo from '../images/glug.svg';
 
 export default () => {
     return <div className="bg-transparent p-3">
-        <span className="text-center mx-auto d-block text-white"><b>Hosted and Created by:</b> GNU Linux Users Group(GLUG)</span>
+        <span className="text-center mx-auto d-block text-white"><b>Hosted and Created by</b> GNU Linux Users Group, NIT Durgapur</span>
         <img src={logo} alt="GLUG" height="50" className="mx-auto d-block mt-2"/>
     </div>
 }
